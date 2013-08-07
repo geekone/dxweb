a2d494ca9be904aa61f4f8e462a936ff8c6b287c
ajaxj <ajaxj@qq.com>
2013-08-07 11:33:17 +0800

    init projects

---
+++ routes/admin.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 13-8-7
+ * Time: 上午11:24
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.index = function(req,res,next){
+    return res.send("this is admin");
+}
